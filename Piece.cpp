952b4408c772736760b1ac7674e3f0ba6feb1e21
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-08 00:36:11 -0400

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -1,49 +1,15 @@
 #include "Piece.h"
 
-
 Piece::Piece() {
     name = "";
     color = "";
     symbol = "";
 }
 
-Piece::Piece(std::string myName, std::string myColor) {
+Piece::Piece(std::string myName, std::string myColor, std::string mySymbol) {
     name = myName;
     color = myColor;
-    if (color == "white") {
-        if (name == "pawn")
-            symbol = "P";
-        else if (name == "bishop")
-            symbol = "B";
-        else if (name == "knight")
-            symbol = "N";
-        else if (name == "rook")
-            symbol = "R";
-        else if (name == "queen")
-            symbol = "Q";
-        else if (name == "king")
-            symbol = "K";
-        else
-            symbol = " ";
-    }
-    else if (color == "black") {
-        if (name == "pawn")
-            symbol = "P";
-        else if (name == "bishop")
-            symbol = "B";
-        else if (name == "knight")
-            symbol = "N";
-        else if (name == "rook")
-            symbol = "R";
-        else if (name == "queen")
-            symbol = "Q";
-        else if (name == "king")
-            symbol = "K";
-        else
-            symbol = " ";
-    }
-    else
-        symbol = "no color";
+    symbol = mySymbol;
 }
 
 void Piece::setName(std::string myName) {
