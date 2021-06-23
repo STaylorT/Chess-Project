14b41993c1000c75b220ec29797ae17f13fb2c56
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-23 03:40:18 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,27 @@
 // This program simulates common chess openings.
 #include "Board.h"
 #include "Piece.h"
+#include "Game.h"
 #include <iostream>
 #include <iomanip>
 #include <fcntl.h>
+#include <fstream>
 #include <io.h>
 #include <stdio.h>
 using namespace std;
 
 int main(){
 	Board board1 = Board();
+	int size = 0;
+	cout << "Enter size of board: ";
+	cin >> size;
+	board1.setSize(size);
 	board1.printBoard();
+	string move;
+	cout << "Enter a move: (E4 Only):: ";
+	cin >> move;
+	board1.processMove(move);
+	board1.printBoard();
+
 	return 0;
 }
