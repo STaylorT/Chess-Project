edb643937b114e9bf7f23794da21833c48f82520
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-28 19:43:01 -0400

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -6,16 +6,13 @@ class Piece {
 private:
     std::string name;
     std::string color;
-    std::string symbol;
 public:
     Piece();
-    Piece(std::string myName, std::string myColor, std::string mySymbol);
+    Piece(std::string myName, std::string myColor);
     void setColor(std::string name);
     void setName(std::string name);
     std::string getColor();
     std::string getName();
-    std::string getSymbol();
-
 };
 
 #endif
