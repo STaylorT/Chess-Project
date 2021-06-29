1f493a215d2da5cbc87621814814ca328af44714
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-28 21:09:23 -0400

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -6,13 +6,17 @@ class Piece {
 private:
     std::string name;
     std::string color;
+    std::string square;
 public:
     Piece();
     Piece(std::string myName, std::string myColor);
-    void setColor(std::string name);
-    void setName(std::string name);
+    Piece(std::string myName, std::string myColor, std::string mySquare);
+    void setColor(std::string myColor);
+    void setName(std::string myName);
+    void setSquare(std::string mySquare);
     std::string getColor();
     std::string getName();
+    std::string getSquare();
 };
 
 #endif
