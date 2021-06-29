c25f979cfdef3d7d1d4a493c46cea975547a0e3f
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-28 21:09:10 -0400

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -10,15 +10,27 @@ Piece::Piece(std::string myName, std::string myColor) {
     color = myColor;
 }
 
+Piece::Piece(std::string myName, std::string myColor, std::string mySquare) {
+    name = myName;
+    color = myColor;
+    square = mySquare;
+}
+
 void Piece::setName(std::string myName) {
     name = myName;
 }
 void Piece::setColor(std::string myColor) {
     color = myColor;
 }
+void Piece::setSquare(std::string mySquare) {
+    square = mySquare;
+}
 std::string Piece::getName() {
     return name;
 }
 std::string Piece::getColor() {
     return color;
+}
+std::string Piece::getSquare() {
+    return square;
 }
