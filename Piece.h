4a397e8ba2867643b85f0ff5189890c7d0799de5
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-30 00:36:40 -0400

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -7,16 +7,21 @@ private:
     std::string name;
     std::string color;
     std::string square;
+    int file;
+    int rank;
 public:
     Piece();
     Piece(std::string myName, std::string myColor);
     Piece(std::string myName, std::string myColor, std::string mySquare);
+    Piece(std::string myName, std::string myColor, int myFile, int myRank);
     void setColor(std::string myColor);
     void setName(std::string myName);
     void setSquare(std::string mySquare);
     std::string getColor();
     std::string getName();
     std::string getSquare();
+    int getRank();
+    int getFile();
 };
 
 #endif
