338fb5df113c388564c27e53890b24469a5039d9
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-06-30 00:35:39 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,9 +1,15 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <vector>
+#include <map>
 #include <string>
+
 const int numFiles = 8;
 const int numRanks = 8;
+
+enum file {a, b, c,d, e, f, g, h};
+
+std::map<char, file> filemap = boost::assign::map_list_of('A', file::a)('B', file::b)('C', file::c)('D', file::d)('E', file::e)('F', file::f)('G', file::g)('H', file::h);
 
 #include "Piece.h"
 
@@ -19,7 +25,8 @@ public:
     void setBoard();
     void printRank(std::string row, int currRank, int squareHeight);
     void setSize(int mySize);
-    void processMove(std::string myMove);
+    int parseMove(std::string myMove);
+    void processMove(std::vector<char> myVec, std::string myMove);
 
 };
 
